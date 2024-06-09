b55b76267c263f89f144fde9025b2eb40ac17b5d
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2024-06-09 20:59:00 +0900

    打刻ページ：出退勤打刻時のstatus更新

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use App\Models\Work;
 use Auth;
 use Carbon\Carbon;
@@ -20,51 +21,53 @@ class AttendanceController extends Controller
     // 打刻処理
     public function punch(Request $request) {
         $auths = Auth::user();
-        $name = $auths->name;
 
         if ($request->has('punch_in')) {
-            $this->punchIn($auths->id);
+            $this->punchIn($auths);
         }elseif ($request->has('punch_out')) {
-            $this->punchOut($auths->id);
+            $this->punchOut($auths);
         }elseif ($request->has('rest_in')) {
-            $this->restIn($auths->id);
+            $this->restIn($auths);
         }elseif ($request->has('rest_out')) {
-            $this->restOut($auths->id);
+            $this->restOut($auths);
         }
 
-        return view('punch', compact('name'));
+        $name = $auths->name;
+        $status = $auths->status;
+        return view('punch', compact(['name', 'status']));
     }
 
     // 勤務開始
-    private function punchIn($user_id) {
+    private function punchIn($user) {
         $now = Carbon::now();
 
         $work = [
-            "user_id" => $user_id,
+            "user_id" => $user->id,
             "work_on" => $now->format('Y-m-d'),
             "began_at" => $now->format('H:i:s'),
         ];
-
         Work::create($work);
+        $user->update(['status' => '1']);
     }
 
     // 勤務終了
-    private function punchOut($user_id) {
+    private function punchOut($user) {
         $now = Carbon::now();
 
         $work = ["finished_at" => $now->format('H:i:s')];
-        Work::where('user_id', $user_id)
+        Work::where('user_id', $user->id)
             ->where('work_on', $now->format('Y-m-d'))
             ->update($work);
+        $user->update(['status' => 0]);
     }
 
     // 休憩開始
-    private function restIn($user_id) {
+    private function restIn($user) {
 
     }
 
     // 休憩終了
-    private function restOut($user_id) {
+    private function restOut($user) {
 
     }
 }
