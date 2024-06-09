28b14eb8112f8628b405109bdd56f020e4d86a8a
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2024-06-09 14:25:25 +0900

    打刻ページ：view、css、実装完了

--- src/resources/views/punch.blade.php
+++ src/resources/views/punch.blade.php
@@ -5,5 +5,16 @@
 @endsection
 
 @section('content')
-punch
+<div class="container">
+    <div class="container__heading">nameさんお疲れ様です！</div>
+    <div class="container__content">
+        <form action="/" class="punch-form" method="post">
+            @csrf
+            <input type="submit" class="form__input" name="punch_in" value="勤務開始">
+            <input type="submit" class="form__input" name="punch_out" value="勤務終了">
+            <input type="submit" class="form__input" name="rest_in" value="休憩開始">
+            <input type="submit" class="form__input" name="rest_out" value="休憩終了">
+        </form>
+    </div>
+</div>
 @endsection
