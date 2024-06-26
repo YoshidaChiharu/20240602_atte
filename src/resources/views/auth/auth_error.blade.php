8870bf151b0f600ad306e1e3251ba779e4660d13
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2024-06-26 15:54:01 +0900

    認証エラーページ：view、css作成

---
+++ src/resources/views/auth/auth_error.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/auth/auth_error.css') }}">
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="container__heading">ログイン認証エラー</div>
+    <div class="container__content">
+        <p class="error-text">{{ $message }}</p>
+        <div class="login">
+            <p class="login-text">再度ログインからお試しください</p>
+            <a href="/login" class="login-link">ログイン</a>
+        </div>
+    </div>
+</div>
+@endsection
